8f5c91122b70136f777f9ad93948da1714210eae
krasilnikov30 <bkrasilnikov3006@gmail.com>
2024-09-17 16:06:06 +0300

    Написал программу flip-case

--- lab2/flipCase/flip-case.cpp
+++ lab2/flipCase/flip-case.cpp
@@ -1,6 +1,13 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <optional>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fstream>
+#include <ranges>
+#include <bits/ranges_algo.h>
+
 
 struct Args
 {
@@ -9,37 +16,91 @@ struct Args
 
 std::optional<Args> ParseArgs(int argc, char* argv[])
 {
-    if (argc < 3)
+    if (argc < 2)
     {
         std::cout << "Invalid argument count\n"
-                  << "Usage: expand_template.exe <inputFile.txt> <outputFile.txt> [<param> <value> [<param> <value> ...]]\n";
+                  << "Usage: flip-case.exe <input1.txt> [<input2.txt> ...]]" << std::endl;
         return std::nullopt;
     }
     Args args;
-    args.inputFilePath = argv[1];
-    args.outputFilePath = argv[2];
-    TemplateParams params;
-    for (size_t i = 3; i <= argc - 2; i += 2)
+    for (size_t i = 1; i <= argc - 1; i++)
     {
-        params.emplace(argv[i], argv[i + 1]);
+        args.files.emplace_back(argv[i]);
     }
-    args.params = params;
 
     return args;
 }
 
-int main(int argc, char* argv[])
+
+
+void FlipCaseFile(std::ifstream& input, std::ofstream& output)
 {
-    auto args = ParseArgs(argc, argv);
+	char ch;
+	while (input.get(ch))
+	{
+		if (!output.put(std::toupper(ch)))
+		{
+			throw std::runtime_error("Failed to save data on disk");
+		}
+	}
+}
+
+void HandleChildProcess(std::string const& fileName)
+{
+	const auto selfPid = getpid();
+	std::cout << "Process " << selfPid << " is processing " << fileName << std::endl;
+	auto input = std::ifstream(fileName);
+	auto output = std::ofstream(fileName + ".out");
+	FlipCaseFile(input, output);
+	std::cout << "Process " << selfPid << " has finished writing to " << fileName << ".out" << std::endl;
+}
+
+void WaitChildProcesses(std::vector<pid_t>& childPids)
+{
+	// сделать через while (Исправлено)
+	while (!empty(childPids))
+	{
+		auto childPid = waitpid(-1, nullptr, 0);
+		std::erase(childPids, childPid);
+		if (childPid == -1)
+		{
+			std::cout << "waitpid failed" << std::endl;
+			return;
+		}
+		std::cout << "Child process " << childPid << " is over" << std::endl;
+	}
+}
+
+int main(const int argc, char* argv[])
+{
+	const auto args = ParseArgs(argc, argv);
     if (!args)
     {
         return EXIT_FAILURE;
     }
 
-    if (!CopyAndExpandWithParams(args->inputFilePath, args->outputFilePath, args->params))
-    {
-        return EXIT_FAILURE;
-    }
+	auto files = args->files;
+	pid_t pid = getpid();
+	std::vector<pid_t> childPids;
+	for (auto fileIt = files.begin(); fileIt != files.end() && pid != 0; ++fileIt)
+	{
+		pid = fork();
+		childPids.emplace_back(pid);
+		if (pid == -1)
+		{
+			std::cout << "fork failed" << std::endl;
+			return EXIT_FAILURE;
+		}
+		if (pid == 0)
+		{
+			HandleChildProcess(*fileIt);
+		}
+	}
+
+	if (pid != 0)
+	{
+		WaitChildProcesses(childPids);
+	}
 
     return EXIT_SUCCESS;
 }
