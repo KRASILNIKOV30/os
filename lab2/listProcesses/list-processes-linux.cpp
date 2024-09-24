61f3aeba1c3a02d949fdb0304559bcaae3b794d0
krasilnikov30 <bkrasilnikov3006@gmail.com>
2024-09-24 12:52:38 +0300

    Начал делать list-processes-linux.cpp

--- lab2/listProcesses/list-processes-linux.cpp
+++ lab2/listProcesses/list-processes-linux.cpp
@@ -1,9 +1,41 @@
+#include <filesystem>
+#include <fstream>
 #include <iostream>
+#include <bits/ranges_algo.h>
 
-#include <sys/ptrace.h>
+namespace fs = std::filesystem;
 
 int main()
 {
-    auto val = ptrace(PTRACE_PEEKDATA, 0, 0);
-}
+	std::string dirPath("/proc");
 
+	for (const auto& fileEntry : fs::directory_iterator(dirPath))
+	{
+
+		if (!is_directory(fileEntry))
+		{
+			continue;
+		}
+		auto filename = fileEntry.path().filename().string();
+		if(!std::ranges::all_of(filename, [](const char c){ return std::isdigit(c); }))
+		{
+			continue;
+		}
+		std::ifstream file(fileEntry.path().string() + "/status");
+		std::cout << filename << ": " << std::endl;
+		std::string line;
+		while (std::getline(file, line))
+		{
+			std::string field = line.substr(0, line.find(' '));
+			if (field == "Name:")
+			{
+				std::cout << line << std::endl;
+			}
+			if (field == "VmSize:")
+			{
+				std::cout << line << std::endl;
+			}
+		}
+		std::cout << std::endl;
+	}
+}
