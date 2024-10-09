8fc1cf50ce72a12bafd08225413aa32e34f0b1f8
Bogdan Krasilnikov <bogdan.krasilnikov@ispring.com>
2024-10-09 15:21:52 +0300

    Иван Грозный убил своего сына

--- lab3/kill-child.cpp
+++ lab3/kill-child.cpp
@@ -1,8 +1,68 @@
 #include <iostream>
-#include <signal.h>
+#include <csignal>
 #include <unistd.h>
 #include <string>
 #include <vector>
+#include <atomic>
+#include <chrono>
+#include <thread>
+#include <sys/wait.h>
+
+std::atomic_flag exitRequested;
+
+void SignalHandler(const int signum)
+{
+	if (signum == SIGTERM)
+	{
+		exitRequested.test_and_set();
+	}
+}
+
+void PrintAliveMessages()
+{
+	while (!exitRequested.test())
+	{
+		std::this_thread::sleep_for(std::chrono::seconds(5));
+		if (!exitRequested.test())
+		{
+			std::cout << "Child process " << getpid() << " is still alive" << std::endl;
+		}
+		else
+		{
+			return;
+		}
+	}
+
+}
+
+void ChildProcessAction()
+{
+	if (std::signal(SIGTERM, SignalHandler) == SIG_ERR)
+	{
+		std::cerr << "std::signal failed" << std::endl;
+	}
+	PrintAliveMessages();
+}
+
+bool KillChild(std::vector<pid_t>& childPids)
+{
+	if (childPids.empty())
+	{
+		return false;
+	}
+	auto pidToKill = childPids.back();
+	if (kill(pidToKill, SIGTERM) != 0)
+	{
+		std::cerr << "kill failed" << std::endl;
+	}
+	if (waitpid(pidToKill, nullptr, 0) != -1)
+	{
+		std::cerr << "Child process " << pidToKill << " was killed" << std::endl;
+		childPids.pop_back();
+	}
+
+	return true;
+}
 
 int main()
 {
@@ -16,10 +76,26 @@ int main()
 		if (command == "spawn")
 		{
 			pid = fork();
+			if (pid != 0)
+			{
+				std::cout << "Child process " << pid << " have been spawned" << std::endl;
+				childPids.push_back(pid);
+			}
 		}
 		else if (command == "kill")
 		{
-			kill(childPids.back(), SIGTERM);
+			if (!KillChild(childPids))
+			{
+				std::cerr << "No children to kill" << std::endl;
+			}
+
 		}
 	}
+
+	if (pid == 0)
+	{
+		ChildProcessAction();
+	}
+
+	return EXIT_SUCCESS;
 }
